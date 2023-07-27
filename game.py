f8748252f46a30b3614c4d1e06ff24f76e352c8e
unknown <961099356@qq.com>
2023-07-28 00:36:31 +0800

    modified:   game.py 	new file:   test_code_1.py 	new file:   "\346\270\270\346\210\217\350\256\276\350\256\241\346\241\206\346\236\266.png"

--- game.py
+++ game.py
@@ -1,27 +1,56 @@
 from random import shuffle
-from card import PhysicalAttackCard,MagicAttackCard,MentalAttackCard
+from card import PhysicalAttackCard, MagicAttackCard, MentalAttackCard
+import abc
 
-class BaseStage:
-    def __init__(self):
+
+# 基础阶段类
+class BaseStage(metaclass=abc.ABCMeta):
+    def __init__(self, player):
+        self.player = player
+
+    @abc.abstractclassmethod
+    def start_stage(self, player):
         pass
 
-#牌堆类
+    @abc.abstractclassmethod
+    def end_stage(self, palyer):
+        pass
+
+
+# 摸牌阶段实现
+class DrawStage(BaseStage):
+    super().__init__()
+
+    def start_stage(self, drawpile, player):
+        a = []
+        a.append(drawpile.pop())
+        a.append(drawpile.pop())
+        player.hand_sequence.extend(a)
+
+
+# 牌堆类
 class CardPile:
     def __init__(self):
-        self.card_list=[]
-        
-        
+        self.card_list = []
+
+
 class DrawPile(CardPile):
     def __init__(self) -> None:
-        self.card_list = [PhysicalAttackCard(),MagicAttackCard(),MentalAttackCard()]*5
+        self.card_list = [
+            PhysicalAttackCard(),
+            MagicAttackCard(),
+            MentalAttackCard(),
+        ] * 5
         shuffle(self.card_list)
 
 
+# 回合类
 class Round:
     def __init__(self):
         pass
 
-class Game:
-    def __init__(self,*players):
-        self.player_list=[*players]
 
+# 游戏类
+class Game:
+    def __init__(self, *players):
+        self.player_list = [*players]
