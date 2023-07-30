1ab6de959d06855136c16ab7c8416a92c81aa5e4
nocturne123 <961099356@qq.com>
2023-07-30 23:56:05 +0800

    Update test_code_1.py

--- test_code_1.py
+++ test_code_1.py
@@ -1,7 +1,21 @@
-a = [1, 2, 3]
-b = []
-for i in range(2):
-    
-    b.append(a.pop())
-print(a)
-print(b)
+def e_a():
+    for i in range(3):
+        print(f'a-{i}')
+        yield i
+
+def e_b():
+    for i in range(5):
+        print(f'b-{i}')
+        yield i
+
+def e_c():
+    for i in range(7):
+        print(f'c-{i}')
+        yield i
+
+def output():
+    yield from e_a
+    yield from e_b
+    yield from e_c
+
+output()
