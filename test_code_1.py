ea470cd511998b4bf8af7718796c448c245d0f54
nocturne123 <961099356@qq.com>
2023-07-31 13:57:31 +0800

    Update test_code_1.py

--- test_code_1.py
+++ test_code_1.py
@@ -1,21 +1,52 @@
-def e_a():
-    for i in range(3):
-        print(f'a-{i}')
+'''
+def index_words_iter(text):
+    if text:
+        yield 0
+    for index,letter in enumerate(text):
+        if letter == ' ':
+            yield index +1
+
+address = 'Four score and seven years ago...'
+
+it = index_words_iter(address)
+print(next(it))
+print(next(it))
+
+def move(period ,speed):
+    for _ in range(period):
+        yield speed
+
+def pause(delay):
+    for _ in range(delay):
+        yield 0
+
+def render(delta):
+    print(f'Delta:{delta:.1f}')
+
+def run(func):
+    for delta in func():
+        render(delta)
+
+def animate_composed():
+    yield from move(4,5.0)
+    yield from pause(3)
+    yield from move(2,3.0)
+
+run(animate_composed)
+'''
+def count_from1():
+    for i in range(10):
         yield i
 
-def e_b():
-    for i in range(5):
-        print(f'b-{i}')
-        yield i
+it = iter(count_from1())
 
-def e_c():
-    for i in range(7):
-        print(f'c-{i}')
-        yield i
+it.send(None)
 
-def output():
-    yield from e_a
-    yield from e_b
-    yield from e_c
+print(next(it))
+print(next(it))
 
-output()
+it.send(30)
+
+print(next(it))
+print(next(it))
+
