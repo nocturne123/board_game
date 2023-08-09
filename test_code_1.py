3b017385eae7bbd929b5912f813a49094e2912e0
nocturne123 <961099356@qq.com>
2023-08-09 17:47:59 +0800

    更新设计文档
    将设计文档更新为pptx格式，更适合做思路图，将krita里面的内容迁移过来之后删除krita文件

--- test_code_1.py
+++ test_code_1.py
@@ -1,3 +1,13 @@
-a=4
-a += 1
-print(a)
+class A:
+    def __init__(self):
+        self.name = None
+        self.able = None
+
+    def setup(self):
+        self.add_add =1
+
+a = A()
+print(a.name)
+
+a.setup()
+print(a.add_add)
