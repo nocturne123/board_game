cf7af12e0251c67fb23e888aa0aefdd82db084e2
nocturne123 <961099356@qq.com>
2023-10-26 19:45:26 +0800

    技能测试代码更新

--- test_code_1.py
+++ test_code_1.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 if message == 'a':
     #卡牌生效前的操作
@@ -12,3 +13,34 @@ else:
 
 if message == 'c':
     #卡牌生效后的操作
+=======
+class Skill:
+    def __init__(self, name, damage):
+        self.name = name
+        self.damage = damage
+
+    @staticmethod
+    def give_a(i):
+        i.num += 1
+
+    def hook_in(self, m):
+        m.skill.append(self.give_a)
+
+
+class ent:
+    def __init__(self) -> None:
+        self.skill = []
+        self.num = 0
+
+    def hooks(self):
+        if self.skill:
+            for i in self.skill:
+                i(self)
+
+
+a = ent()
+b = Skill("a", 1)
+b.hook_in(a)
+a.hooks()
+print(a.num)
+>>>>>>> Stashed changes
