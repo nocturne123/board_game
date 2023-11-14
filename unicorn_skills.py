cce00fa6b6aec5ec4c101367a78aabb13075829e
nocturne123 <961099356@qq.com>
2023-11-14 17:56:09 +0800

    日光耀耀技能更新

--- unicorn_skills.py
+++ unicorn_skills.py
@@ -16,12 +16,17 @@ class Sunburst_1(CharacterSkill):
         self.registed = False
 
     def use(self, card, target, discard_pile):
-        """弃置一张牌，给目标挂接一个特效，当目标回血时受到伤害"""
+        """弃置一张牌，给目标挂接一个标记和一个回血时根据标记扣血的特效
+        特效挂接到Hook_After_Healing上"""
         self.player.card_action.discard_card(card, discard_pile)
+        # 打上标记，目标有标记则标记加1，同时将目标记录到affected_list中，技能失效时注销
         if hasattr(target.data, "sunburst_mark"):
             target.data.sunburst_mark += 1
         else:
-            pass
+            setattr(target.data, "sunburst_mark", 1)
+        if self.sunburst_hurt_after_healing not in target.data.Hook_After_Healing:
+            target.data.Hook_After_Healing.append(self.sunburst_hurt_after_healing)
+        self.affected_list.append(target)
 
-    def sunburst_mark(self):
+    def sunburst_hurt_after_healing(self):
         pass
