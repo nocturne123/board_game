5916632d1b563edc1599b7bf7d21963c8222544d
nocturne123 <961099356@qq.com>
2024-03-08 16:13:03 +0800

    Update map_block.py

--- map_block.py
+++ map_block.py
@@ -1,17 +1,22 @@
+"""使用hexlogic库的Hex类，定义地图图块"""
+
 from ENUMS.common_enums import BlockTypeEnum
-from hexutil import Hex
+
+# from hexutil import Hex
+from hexlogic import HexCoords, hex_to_pixel
 import arcade
 
 TILE_SCALING = 1
 
-
-def hex_to_pixel(hex: Hex) -> tuple:
-    """Converts a hex coordinate to a pixel coordinate
-    将六边形坐标转换为像素坐标
-    """
+"""
+def hex_to_pixel(hex: HexCoords) -> tuple:
+    Converts a hex coordinate to a pixel coordinate
+    #将六边形坐标转换为像素坐标
+    
     hex_x = hex[0]
     hex_y = hex[1]
     return (hex_y * -24, hex_x * 13)
+"""
 
 
 class map_block(arcade.Sprite):
@@ -20,8 +25,9 @@ class map_block(arcade.Sprite):
         block_type: BlockTypeEnum = BlockTypeEnum.grass,
         image_x=0,
         image_y=0,
-        hex_x=0,
-        hex_y=0,
+        hex_q=0,
+        hex_r=0,
+        hex_s=0,
     ):
         super().__init__(
             filename="resources/images/fantasyhextiles_v3.png",
@@ -30,22 +36,29 @@ class map_block(arcade.Sprite):
             scale=TILE_SCALING,
             image_x=image_x,
             image_y=image_y,
-            center_x=300 + hex_to_pixel(Hex(hex_x, hex_y))[0],
-            center_y=300 + hex_to_pixel(Hex(hex_x, hex_y))[1],
+            center_x=300
+            + hex_to_pixel(
+                HexCoords(hex_q, hex_r, hex_s), tile_width=13, tile_height=24
+            )[0],
+            center_y=300
+            + hex_to_pixel(
+                HexCoords(hex_q, hex_r, hex_s), tile_width=13, tile_height=24
+            )[1],
         )
-        self.hex_position = Hex(hex_x, hex_y)
+        self.hex_position = HexCoords(hex_x, hex_y)
         self.type: BlockTypeEnum = block_type
 
 
 # 下面是具体的地图图块定义，除了具体城镇，其他图块不用传入block_type参数
 class grass_block(map_block):
-    def __init__(self, hex_x, hex_y):
+    def __init__(self, hex_q, hex_r, hex_s):
         super().__init__(
             block_type=BlockTypeEnum.grass,
             image_x=0,
             image_y=0,
-            hex_x=hex_x,
-            hex_y=hex_y,
+            hex_q=hex_q,
+            hex_r=hex_r,
+            hex_s=hex_s,
         )
 
 
